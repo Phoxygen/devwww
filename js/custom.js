76e283f7dd9809d2bd67956cd6e980b68c82fbe8
Augustin Trancart <augustin.trancart@phoxygen.com>
2015-12-17 12:18:53 +0100

    Fix the "home" and "Phoxygen" links
    The links on the menu when not in index.html were broken.

--- js/custom.js
+++ js/custom.js
@@ -13,7 +13,8 @@ $("#menu-toggle").click(function(e) {
 // Scrolls to the selected menu item on the page
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '')) {
 
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
